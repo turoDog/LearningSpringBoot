d2990ac96471487cebd92b2754c52c198be81983
turod <turoDog@foxmail.com>
2018-08-21 22:41:11 +0800

    《Spring Boot 两种多数据源配置：JdbcTemplate》

---
+++ SpringBoot-MoreDataSource-jdbc/src/main/java/com/nasus/DataSourceConfig.java
@@ -0,0 +1,54 @@
+package com.nasus;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Project Name:LearningSpringBoot <br/>
+ * Package Name:com.nasus <br/>
+ * Date:2018/8/21 22:35 <br/>
+ * <b>Description:</b> TODO: 描述该类的作用 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">nasus</a><br/>
+ * Copyright Notice =========================================================
+ * This file contains proprietary information of Eastcom Technologies Co. Ltd.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 2018 =======================================================
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "primaryDataSource")
+    @Qualifier("primaryDataSource")
+    @ConfigurationProperties(prefix="spring.datasource.primary")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @Qualifier("secondaryDataSource")
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource.secondary")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "primaryJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(
+            @Qualifier("primaryDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "secondaryJdbcTemplate")
+    public JdbcTemplate secondaryJdbcTemplate(
+            @Qualifier("secondaryDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+}
