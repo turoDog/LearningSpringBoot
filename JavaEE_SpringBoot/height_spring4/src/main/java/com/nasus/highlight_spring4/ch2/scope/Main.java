e177df0d100e136ced4133ad760d890c07b041a6
turod <turoDog@foxmail.com>
2018-09-02 21:40:48 +0800

    Spring基本配置-- Bean 的生命周期

---
+++ JavaEE_SpringBoot/height_spring4/src/main/java/com/nasus/highlight_spring4/ch2/scope/Main.java
@@ -0,0 +1,33 @@
+package com.nasus.highlight_spring4.ch2.scope;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Project Name:LearningSpringBoot <br/>
+ * Package Name:com.nasus.highlight_spring4.ch2.scope <br/>
+ * Date:2018/9/2 21:38 <br/>
+ * <b>Description:</b> TODO: 描述该类的作用 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">nasus</a><br/>
+ * Copyright Notice =========================================================
+ * This file contains proprietary information of Eastcom Technologies Co. Ltd.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 2018 =======================================================
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ScopeConfig.class);
+        DemoSingletonService s1 = context.getBean(DemoSingletonService.class);
+        DemoSingletonService s2 = context.getBean(DemoSingletonService.class);
+
+        DemoPrototypeService p1 = context.getBean(DemoPrototypeService.class);
+        DemoPrototypeService p2 = context.getBean(DemoPrototypeService.class);
+
+        System.out.println("s1与s2是否相等：" + s1.equals(s2));
+        System.out.println("p1与p2是否相等：" + p1.equals(p2));
+
+        context.close();
+    }
+}
