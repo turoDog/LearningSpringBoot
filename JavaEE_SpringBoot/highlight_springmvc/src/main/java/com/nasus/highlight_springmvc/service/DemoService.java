54b644115a3c6fa8ece78ec8298091edeb75087c
turod <turoDog@foxmail.com>
2018-09-09 00:51:30 +0800

    Spring mvc 测试用例 service

---
+++ JavaEE_SpringBoot/highlight_springmvc/src/main/java/com/nasus/highlight_springmvc/service/DemoService.java
@@ -0,0 +1,24 @@
+package com.nasus.highlight_springmvc.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Project Name:LearningSpringBoot <br/>
+ * Package Name:com.nasus.highlight_springmvc.service <br/>
+ * Date:2018/9/9 0:48 <br/>
+ * <b>Description:</b> TODO: 描述该类的作用 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">nasus</a><br/>
+ * Copyright Notice =========================================================
+ * This file contains proprietary information of Eastcom Technologies Co. Ltd.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 2018 =======================================================
+ */
+@Service
+public class DemoService {
+
+    public String saySomething(){
+        return "hello";
+    }
+
+}
